d8346ac95d9baa5eb31b59e77acff80ecd0659ed
Nicholas Todoroff <loppy1243@gmail.com>
2021-12-19 21:06:37 -0500

    Unindent module QuadraticForms

--- src/QuadraticForms.jl
+++ src/QuadraticForms.jl
@@ -6,87 +6,89 @@
 ### Is is possible to generically program for both of these cases
 ###     in an ergonomic way?
 module QuadraticForms
-    import Cassette
-    import LinearAlgebra
-    using ..GeomAlg
-    using ..GeomAlg: @unsafe
-    export AbstractQuadraticForm
 
-    abstract type AbstractQuadraticForm{K, N} end
-    Base.@propagate_inbounds (q::AbstractQuadraticForm)(x::Int) = q(x, x)
+import Cassette
+import LinearAlgebra
+using ..GeomAlg
+using ..GeomAlg: @unsafe
+export AbstractQuadraticForm
 
-    function Base.Matrix(q::AbstractQuadraticForm)
-        N = vectorspacedim(q)
-        [@inbounds q(i, j) for i=1:N, j=1:N]
+abstract type AbstractQuadraticForm{K, N} end
+Base.@propagate_inbounds (q::AbstractQuadraticForm)(x::Int) = q(x, x)
+
+function Base.Matrix(q::AbstractQuadraticForm)
+    N = vectorspacedim(q)
+    [@inbounds q(i, j) for i=1:N, j=1:N]
+end
+Base.Matrix{K}(q::AbstractQuadraticForm) where K =
+    convert(Matrix{K}, Matrix(q))
+
+struct Unsafe{K, N} <: AbstractQuadraticForm{K, N}
+    mat::Matrix{K}
+end
+Base.@propagate_inbounds (q::Unsafe)(x::Int) = q.mat[x,x]
+Base.@propagate_inbounds (q::Unsafe)(x::Int, y::Int) = q.mat[x,y]
+Base.Matrix(x::Unsafe) = x.mat
+
+struct Diagonal{K, N} <: AbstractQuadraticForm{K, N}
+    sig::NTuple{N, K}
+end
+
+Base.@propagate_inbounds (q::Diagonal)(x::Int) = q.sig[x]
+Base.@propagate_inbounds (q::Diagonal)(x::Int, y::Int) =
+    x == y ? q.sig[x] : zero(scalarfieldtype(q))
+
+Base.Matrix(q::Diagonal) = LinearAlgebra.Diagonal(collect(q.sig))
+
+struct BasisChange{
+    K, N, QT<:AbstractQuadraticForm{K, N}
+} <: AbstractQuadraticForm{K, N}
+    inner::QT
+    matrix::Matrix{K} # (new <- standard) change-of-basis matrix
+
+    @unsafe function BasisChange{K, N, QT}(inner, matrix) where {
+        K, N, QT<:AbstractQuadraticForm
+    }
+        new{K, N, QT}(inner, matrix)
     end
-    Base.Matrix{K}(q::AbstractQuadraticForm) where K =
-        convert(Matrix{K}, Matrix(q))
+end
+function BasisChange{K, N, QT}(inner::QT, matrix::Matrix{K}) where
+         {K, N, QT<:AbstractQuadraticForm{K, N}}
+    @assert size(matrix) == (N, N)
+    # Technically should be checking for invertibility too...
+    @unsafe BasisChange{K, N, QT}(inner, matrix)
+end
 
-    struct Unsafe{K, N} <: AbstractQuadraticForm{K, N}
-        mat::Matrix{K}
-    end
-    Base.@propagate_inbounds (q::Unsafe)(x::Int) = q.mat[x,x]
-    Base.@propagate_inbounds (q::Unsafe)(x::Int, y::Int) = q.mat[x,y]
-    Base.Matrix(x::Unsafe) = x.mat
+Base.@propagate_inbounds function (q::BasisChange)(x::Int, y::Int)
+    N = vectorspacedim(q)
 
-    struct Diagonal{K, N} <: AbstractQuadraticForm{K, N}
-        sig::NTuple{N, K}
-    end
-
-    Base.@propagate_inbounds (q::Diagonal)(x::Int) = q.sig[x]
-    Base.@propagate_inbounds (q::Diagonal)(x::Int, y::Int) =
-        x == y ? q.sig[x] : zero(scalarfieldtype(q))
-
-    Base.Matrix(q::Diagonal) = LinearAlgebra.Diagonal(collect(q.sig))
-
-    struct BasisChange{
-        K, N, QT<:AbstractQuadraticForm{K, N}
-    } <: AbstractQuadraticForm{K, N}
-        inner::QT
-        matrix::Matrix{K} # (new <- standard) change-of-basis matrix
-
-        @unsafe function BasisChange{K, N, QT}(inner, matrix) where {
-            K, N, QT<:AbstractQuadraticForm
-        }
-            new{K, N, QT}(inner, matrix)
-        end
-    end
-    function BasisChange{K, N, QT}(inner::QT, matrix::Matrix{K}) where
-             {K, N, QT<:AbstractQuadraticForm{K, N}}
-        @assert size(matrix) == (N, N)
-        # Technically should be checking for invertibility too...
-        @unsafe BasisChange{K, N, QT}(inner, matrix)
-    end
-
-    Base.@propagate_inbounds function (q::BasisChange)(x::Int, y::Int)
-        N = vectorspacedim(q)
-
-        @boundscheck if !(1 <= x <= N && 1 <= y <= N)
-            throw(BoundError(q, (x, y)))
-        end
-
-        sum(@inbounds q.matrix[i,x]*q.inner(i,j)*q.matrix[j,y] for i=1:N, j=1:N)
+    @boundscheck if !(1 <= x <= N && 1 <= y <= N)
+        throw(BoundError(q, (x, y)))
     end
 
-    Base.Matrix(q::BasisChange) = transpose(q.matrix)*Matrix(q.inner)*q.matrix
+    sum(@inbounds q.matrix[i,x]*q.inner(i,j)*q.matrix[j,y] for i=1:N, j=1:N)
+end
 
-    function Base.show(io::IO, ::MIME"text/plain", q::AbstractQuadraticForm)
-        K = scalarfieldtype(q)
-        N = vectorspacedim(q)
-        print(io, nameof(typeof(q)), " ", N, "D ", nameof(K), "-form ")
-        terms = []
-        for j in 1:N
-            qj = q(j)
-            if !iszero(qj) push!(terms, "$(qj)X$(j)²") end
-            for i = (j+1):N
-                qij = q(i, j)
-                if !iszero(qij)
-                    push!(push!(terms, "$(qij)X$(i)*X$(j)"))
-                end
+Base.Matrix(q::BasisChange) = transpose(q.matrix)*Matrix(q.inner)*q.matrix
+
+function Base.show(io::IO, ::MIME"text/plain", q::AbstractQuadraticForm)
+    K = scalarfieldtype(q)
+    N = vectorspacedim(q)
+    print(io, nameof(typeof(q)), " ", N, "D ", nameof(K), "-form ")
+    terms = []
+    for j in 1:N
+        qj = q(j)
+        if !iszero(qj) push!(terms, "$(qj)X$(j)²") end
+        for i = (j+1):N
+            qij = q(i, j)
+            if !iszero(qij)
+                push!(push!(terms, "$(qij)X$(i)*X$(j)"))
             end
         end
-        print(io, join(terms, " + "))
-
-        nothing
     end
+    print(io, join(terms, " + "))
+
+    nothing
+end
+
 end # module QuadraticForms
